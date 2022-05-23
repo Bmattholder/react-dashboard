df3a9575141a38638c3c7da14f9d7e74c7a13ee0
Matthew Holder <60249613+Bmattholder@users.noreply.github.com>
2022-05-22 21:47:15 -0500

    add context api (#3)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FiShoppingCart } from 'react-icons/fi';
+import { BsCharLeft } from 'react-icons/bs';
+import { RiNotification3Line } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import avatar from '../data/avatar.jpg';
+import { Cart, Chat, Notification, UserProfile } from '.';
+import { useStateContext } from '../contexts/ContextProvider';
 
 function Navbar() {
-	return (
-		<div>Navbar</div>
-	)
+	const { activeMenu, setActiveMenu } = useStateContext();
+
+  return (
+	<div className='flex justify-between p-2 md:mx-6 relative'>
+		Navbar
+	</div>)
 }
 
-export default Navbar
+export default Navbar;
